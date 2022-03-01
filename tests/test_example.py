42760c5cdd655d7548c5616af1dcf1d77f7ced6c
DzmitrySha <ravenbydev@gmail.com>
2022-03-01 20:19:31 +0300

    added project hexlet-pytest files

---
+++ tests/test_example.py
@@ -0,0 +1,8 @@
+from hexlet_pytest.example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
+
---
+++ tests/test_hexlet_pytest.py
@@ -0,0 +1,5 @@
+from hexlet_pytest import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
