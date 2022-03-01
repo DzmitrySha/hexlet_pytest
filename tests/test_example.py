db9d65fa21e18bc6de22386fb8635a4f275d7edc
DzmitrySha <ravenbydev@gmail.com>
2022-03-01 21:15:13 +0300

    modified .gitignore test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,9 @@
 from hexlet_pytest.example import reverse
 
+
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
 
+
 def test_reverse_for_empty_string():
     assert reverse('') == ''
-
